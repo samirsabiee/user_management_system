db8bcb82f92e397bce55f3ddbbc0b020c6211b14
samsab1995 <samirsabiee@gmail.com>
2021-08-24 16:51:48 +0430

    add roles relationShip

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
+use App\Services\Permissions\Traits\HasRole;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Permission extends Model
 {
     use HasFactory;
+
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class);
+    }
 }
