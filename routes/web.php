685509d66e350207223337ed46c5648da9e4809a
samsab1995 <samirsabiee@gmail.com>
2021-08-24 18:51:43 +0430

    add panel route group

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,3 +19,7 @@ Route::view('/', 'welcome');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['prefix' => 'panel'], function () {
+    Route::get('users', [UserController::class, 'index'])->name('users.index');
+});
