ac3c94f1ede787ce179a9c49aeef276e27f857f0
samsab1995 <samirsabiee@gmail.com>
2021-08-24 16:50:05 +0430

    test my logic code

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Role;
+use Illuminate\Contracts\Support\Renderable;
 
 class HomeController extends Controller
 {
@@ -19,10 +20,13 @@ class HomeController extends Controller
     /**
      * Show the application dashboard.
      *
-     * @return \Illuminate\Contracts\Support\Renderable
+     * @return Renderable
      */
-    public function index()
+    public function index(): Renderable
     {
+        Role::find(1)->givePermissionTo('delete user');
+        auth()->user()->giveRoleTo('admin');
+        dd(auth()->user()->can('add user'));
         return view('home');
     }
 }
