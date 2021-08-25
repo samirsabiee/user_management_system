f416e83036c0c265e2ecaf7a5bab494f69ec5147
samsab1995 <samirsabiee@gmail.com>
2021-08-25 17:05:48 +0430

    add roles list menu link

--- resources/views/partials/panel-menu.blade.php
+++ resources/views/partials/panel-menu.blade.php
@@ -1,5 +1,6 @@
 <div class="card">
     <ul class="list-group list-group-flush">
         <li class="list-group-item"><a href="{{ route('users.index') }}">Users List</a></li>
+        <li class="list-group-item"><a href="{{ route('roles.index') }}">Roles List</a></li>
     </ul>
 </div>
